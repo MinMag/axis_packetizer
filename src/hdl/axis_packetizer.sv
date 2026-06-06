f93a0ba861e53ba7a80492c47806517b2189e3fb
Magnus O'Connor <magnusoco@gmail.com>
2026-06-05 19:44:07 -0500

    init project structure

---
+++ src/hdl/axis_packetizer.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2026 12:36:46 AM
+// Design Name: 
+// Module Name: axis_packetizer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module axis_packetizer(
+
+    );
+endmodule
